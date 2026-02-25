fbf3dd300f7f4b6be77431f72ef2d26314037d88
Sourabh Sharma <Sourabh71234@gmail.com>
2026-02-25 20:18:18 +0530

    created animated hero section

---
+++ script.js
@@ -0,0 +1,24 @@
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".main",
+    start: "top top",
+    end: "bottom 50%", 
+    scrub: 2,
+    markers: true
+  },
+});
+
+tl.to(".layer", {
+    x: "100%",
+    duration: 2,
+  })
+
+  .from(".card",
+    {
+      opacity: 0,
+      y: 50,
+      stagger: 0.3,
+      duration: 1,
+    },
+    "-=1",
+  );
